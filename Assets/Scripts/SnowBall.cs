5080c662c2fd444cdb5ed8ea53a89f1d7b4f9f59
Kanyanat Sermsup <kanyanat.serm@bumail.net>
2025-03-30 04:51:13 +0700

    แก้ไข Snow Ball และ เพิ่มปุ่ม Volume Setting

--- Assets/Scripts/SnowBall.cs
+++ Assets/Scripts/SnowBall.cs
@@ -1,18 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SnowBall : MonoBehaviour
 {
-    public int damage = 10; 
+    public int damage = 3;
+    public float damageInterval = 0.2f; 
+    private Dictionary<GameObject, float> lastDamageTime = new Dictionary<GameObject, float>();
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerController CurrentHp = collision.gameObject.GetComponent<PlayerController>();
-            if (CurrentHp != null)
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
             {
-                CurrentHp.TakeDamage(damage);
+                float lastTime;
+                if (!lastDamageTime.TryGetValue(collision.gameObject, out lastTime))
+                {
+                    lastTime = 0;
+                }
+
+                if (Time.time - lastTime >= damageInterval)
+                {
+                    player.TakeDamage(damage);
+                    lastDamageTime[collision.gameObject] = Time.time;
+                }
             }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // ลบข้อมูลเมื่อผู้เล่นออกจากการชน เพื่อให้โจมตีใหม่ทันทีเมื่อลงไปชนอีกครั้ง
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            lastDamageTime.Remove(collision.gameObject);
         }
     }
 }
